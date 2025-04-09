69ffc247c27b86148fbdefeb04eb0b47436c810e
priyankagoti25 <goti.priyanka25@gmail.com>
2025-04-09 18:03:06 +0530

    common middleware and api error handling code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {PORT} from "./constants.js";
+import {app} from "./app.js";
 
 dotenv.config({path: './env'})
 
 connectDB()
+    .then(()=>{
+        app.listen(PORT,()=>{
+            console.log(`Server is running on http://localhost:${PORT}`)
+        })
+    })
+    .catch((err)=>{
+        console.log("MongoDB connection Failed ", err)
+    })
 
 
 
