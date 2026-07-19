c8a385e0bbdc38427385f104bf66ee68a3d097a6
priyankagoti25 <goti.priyanka25@gmail.com>
2026-07-19 23:52:35 +0530

    dummy changes

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -6,8 +6,6 @@ import {
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const subscriptionRoutes = Router();
 
-//I am adding this line to check docpulse again with ngrok live url
-
 subscriptionRoutes
   .route("/subscribe/:channelId")
   .post(verifyJWT, subscribeChannel);
